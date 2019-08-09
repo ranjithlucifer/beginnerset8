580b513aab9b48e2063905a1bc726245c701664b
ranjithlucifer <51074137+ranjithlucifer@users.noreply.github.com>
2019-08-09 17:27:48 +0530

    Update perfectsquare.py

--- perfectsquare.py
+++ perfectsquare.py
@@ -1,9 +1,10 @@
 import math
 n=list(map(int,input().split()))
 c=n[0]*n[1]
-a=int(math.sqrt(c))
-if a==(c/a):
-    print("yes")
-else:
-    print("no")
+if c!=0:
+    a=int(math.sqrt(c))
+    if a==(c/a):
+        print("yes")
+    else:
+        print("no")
 
